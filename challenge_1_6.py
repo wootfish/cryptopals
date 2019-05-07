1a1c95a9a6f798b1c2965b24e80716d620d27afa
Eli Sohl <elisohl@gmail.com>
2019-05-07 03:17:17 -0400

    challenge 1.6 done

--- challenge_1_6.py
+++ challenge_1_6.py
@@ -3,50 +3,26 @@ Set 1, Challenge 6
 """
 
 
-from challenge_1_2 import bytes_xor
-from challenge_1_5 import repeating_key_xor
-import challenge_1_3
-
 import itertools
+from pprint import pprint
+from typing import List, Tuple
+
+import challenge_1_3
+from challenge_1_5 import repeating_key_xor
+from challenge_1_6_util import hamming_distance
 
 
-def get_hamming_weights():
-    """
-    Generates a lookup table for the hamming weight of every byte. Computing
-    these on-demand is easy too, but a lookup table is faster & simpler :)
-    """
-
-    weights = {0: 0}
-    pow_2 = 1
-    for _ in range(8):
-        for k, v in weights.copy().items():
-            weights[k+pow_2] = v+1
-        pow_2 <<= 1
-    return weights
-
-
-weights = get_hamming_weights()
-
-
-def hamming_distance(a: bytes, b: bytes):
-    # the hamming distance between two bytestrings is equal to the hamming
-    # weight of their xor
-    return sum(weights[byte] for byte in bytes_xor(a, b))
-
-
-MAX_KEYSIZE_GUESS = 32  # raise this if keysize guesses aren't panning out
-def guess_keysize(ciphertext: bytes, guesses: int = 1):
-    max_keysize = min(MAX_KEYSIZE_GUESS, len(ciphertext) // 4)
-
-    def get_score(size):
-        chunks = (ciphertext[:size],
-                  ciphertext[size:2*size],
-                  ciphertext[2*size:3*size],
-                  ciphertext[3*size:4*size])
+MAX_KEYSIZE = 40
+def guess_keysize(ct: bytes, guesses: int = 1) -> List[Tuple[float, int]]:
+    def get_score(size: int) -> int:
+        chunks = (ct[:size],
+                  ct[size:2*size],
+                  ct[2*size:3*size],
+                  ct[3*size:4*size])
         avg = sum(hamming_distance(a,b) for a,b in itertools.combinations(chunks, 2)) / 12
         return avg / size
 
-    scores = [(get_score(size), size) for size in range(1, max_keysize+1)]
+    scores = [(get_score(size), size) for size in range(1, MAX_KEYSIZE+1)]
     scores.sort()
     return scores[:guesses]
 
@@ -78,14 +54,14 @@ if __name__ == "__main__":
     ciphertext = base64.b64decode(b64)
 
     keysizes = guess_keysize(ciphertext, 5)
-    #print("Key size guesses:", keysizes)
+    print("Key size guesses (confidence, size):")
+    pprint(keysizes)
+    print()
 
-    challenge_1_3.top_letters = set(ord(ch) for ch in "etaoinshrdluETAOINSHRDLU")  # expand set of target characters for xor cracker
+    candidates = [crack_repeating_key_xor(ciphertext, guess)
+                  for score, guess in keysizes]
+    candidates.sort()
+    top_key = candidates[0][1]
 
-    best_confidence = float('inf')
-    best_guess = None
-
-    for confidence, guess in keysizes:
-        confidence, guess = crack_repeating_key_xor(ciphertext, guess)
-        print(repeating_key_xor(guess, ciphertext))
-        input()
+    print("Top guess: key =", top_key, "plaintext = \\")
+    print(repeating_key_xor(top_key, ciphertext).decode("ascii"))
