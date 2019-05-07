143fb015c880c5bb6243e09576e000a921ba5ccc
Eli Sohl <elisohl@gmail.com>
2019-05-06 22:05:26 -0400

    added challenge_1_2.py

---
+++ challenge_1_2.py
@@ -0,0 +1,25 @@
+"""
+Set 1, Challenge 2
+"""
+
+
+def bytes_xor(a: bytes, b: bytes):
+    return b''.join(
+        bytes([byte_1 ^ byte_2])
+        for byte_1, byte_2 in zip(a, b)
+    )
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 3:
+        sys.exit("Usage: python3 1_2.py hex morehex")
+
+    try:
+        a = bytes.fromhex(sys.argv[1])
+        b = bytes.fromhex(sys.argv[2])
+    except ValueError:
+        sys.exit("Error. Hex input may be malformed. Please try again.")
+
+    result = bytes_xor(a, b)
+    print(result.hex())
