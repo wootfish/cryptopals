1d1dcd48976eb9639c362d094091714bbede2a6c
Eli Sohl <elisohl@gmail.com>
2019-05-07 06:09:10 -0400

    added strip_pkcs7 function to challenge_09.py

--- challenge_09.py
+++ challenge_09.py
@@ -3,6 +3,11 @@ def pkcs7(b: bytes, block_size: int = 16) -> bytes:
     return b + bytes([gap_size] * gap_size)
 
 
+def strip_pkcs7(b: bytes) -> bytes:
+    num = b[-1]
+    return b[:-num]
+
+
 if __name__ == "__main__":
     plaintext = b'YELLOW SUBMARINE'
     block_size = 20
