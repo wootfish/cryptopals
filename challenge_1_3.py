a6d09fca538101732ee118f10f40b3b08c24b005
Eli Sohl <elisohl@gmail.com>
2019-05-07 03:16:45 -0400

    overhauled challenge 1.3 plaintext scoring system & added some notes on the new method

--- challenge_1_3.py
+++ challenge_1_3.py
@@ -1,26 +1,66 @@
 """
 Set 1, Challenge 3
 
-Scores 
+Note: There's a subtle reason why the get_candidate_score function below only
+counts lowercase letters. You might think you'd get better results from
+counting uppercase letters as well. However, there is a quirk of ASCII that we
+have to account for: each ASCII letter's lowercase and uppercase forms differ
+by exactly one bit. As a consequence, for any XOR cipher key producing
+lowercase text there is a key differing by precisely one bit which will produce
+the same plaintext but with inverted case (and with almost all punctuation
+replaced by garbage characters). If lowercase and uppercase letters are
+weighted equally, these related keys are equally likely to be discovered.
+
+There are a number of hacks that could get around this:
+
+    * Check the relative frequencies of lowercase and uppercase text in your
+      output, flip the appropriate key bit(s) if uppercase outnumbers lowercase.
+    * Add in frequency counts for punctuation, as most punctuation characters
+      become corrupted by this bit-flip.
+    * Skip some candidate plaintexts using a blacklist of some or all non-ASCII
+      characters (could improve performance, at the cost of misinterpreting any
+      weird plaintexts)
+    * Create adjusted frequency counts for uppercase letters and score based on
+      these.
+    * Assume the bulk of your ciphertexts' body text will be lowercase and opt
+      to just ignore uppercase letters entirely (figuring the error terms the
+      text's uppercase letters add to each lowercase letter's frequency counts
+      will be roughly equal, meaning the relative frequencies of lowercase
+      letters shouldn't be thrown too far off).
+
+All of these involve some degree of compromise. Since none are perfect, I just
+chose the one that seemed simplest: ignoring uppercase completely.
 """
 
 
 from challenge_1_2 import bytes_xor
+from typing import Tuple
 
 
-top_letters = set(ord(ch) for ch in "etoainETOAIN")
+# derived from http://practicalcryptography.com/media/cryptanalysis/files/english_monograms.txt
+frequencies = {'a': 0.0855, 'b': 0.0160, 'c': 0.0316, 'd': 0.0387, 'e': 0.1209,
+               'f': 0.0218, 'g': 0.0209, 'h': 0.0496, 'i': 0.0732, 'j': 0.0022,
+               'k': 0.0081, 'l': 0.0420, 'm': 0.0253, 'n': 0.0717, 'o': 0.0747,
+               'p': 0.0206, 'q': 0.0010, 'r': 0.0633, 's': 0.0673, 't': 0.0894,
+               'u': 0.0268, 'v': 0.0106, 'w': 0.0182, 'x': 0.0019, 'y': 0.0172,
+               'z': 0.0011}
 
 
-def get_candidate_score(candidate: bytes):
+def get_candidate_score(candidate: bytes) -> float:
+    # lower scores are better
     score = 0
-    for ch in candidate:
-        if ch in top_letters:
-            score += 1
+    l = len(candidate)
+
+    for letter, frequency in frequencies.items():
+        count = candidate.count(ord(letter))  # don't even worry about counting uppercase letters (see note above)
+        err = abs(frequency - count / l)
+        score += err
+
     return score
 
 
-def crack_xor_cipher(ciphertext: bytes):
-    best_score = -1
+def crack_xor_cipher(ciphertext: bytes) -> Tuple[float, bytes, bytes]:
+    best_score = float('inf')
     best_key = None
     best_plaintext = None
 
@@ -29,7 +69,7 @@ def crack_xor_cipher(ciphertext: bytes):
         candidate_plaintext = bytes_xor(ciphertext, bytes([candidate_key]*l))
         score = get_candidate_score(candidate_plaintext)
 
-        if score > best_score:
+        if score < best_score:
             best_score = score
             best_key = candidate_key
             best_plaintext = candidate_plaintext
