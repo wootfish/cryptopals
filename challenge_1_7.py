1afbc6353ef8482201d3ff0b33f17100f28cdf80
Eli Sohl <elisohl@gmail.com>
2019-05-07 03:30:33 -0400

    added 1.7 (& requirements.txt to document dependency on pycrypto)

---
+++ challenge_1_7.py
@@ -0,0 +1,23 @@
+# depends on library pycrypto
+
+
+from Crypto.Cipher import AES
+
+
+def aes_decrypt(key: bytes, ciphertext: bytes) -> bytes:
+    cipher = AES.new(key, AES.MODE_ECB)
+    return cipher.decrypt(ciphertext)
+
+
+if __name__ == "__main__":
+    import sys
+    import base64
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python3 1_7.py filename")
+
+    with open(sys.argv[1]) as f:
+        b64 = f.read()
+    ciphertext = base64.b64decode(b64)
+    plaintext = aes_decrypt(b'YELLOW SUBMARINE', ciphertext)
+
+    print(plaintext.decode('ascii'))  # will show 4 trailing garbage bytes at the end from block padding
