6f77cb73cc12cb789c6109c2053ea0db6b40194f
Eli Sohl <elisohl@gmail.com>
2019-05-07 05:20:09 -0400

    added challenge 12 (this was a really fun one)

---
+++ challenge_12.py
@@ -0,0 +1,71 @@
+from itertools import count
+from base64 import b64decode
+from typing import Callable
+from os import urandom
+
+from Crypto.Cipher import AES
+
+from challenge_09 import pkcs7
+from challenge_11 import detector
+
+
+postfix = b64decode("""
+Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg
+aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq
+dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg
+YnkK
+""")
+
+key = urandom(16)
+
+
+def enc(plaintext: bytes) -> bytes:
+    plaintext = pkcs7(plaintext + postfix)
+    cipher = AES.new(key, AES.MODE_ECB)
+    return cipher.encrypt(plaintext)
+
+
+def discover_block_size_and_postfix_length(enc: Callable[[bytes], bytes]) -> int:
+    # we need to know the postfix length to have a clean termination condition
+    # for the decryption loop, but taking len(postfix) feels like cheating, so
+    # this function just figures it out in tandem w/ finding the block size
+    l = len(enc(b'A'))
+    for i in count(2):
+        l2 = len(enc(b'A'*i))
+        if l2 > l:
+            block_size = l2 - l
+            postfix_len = l - i
+            return block_size, postfix_len
+
+
+if __name__ == "__main__":
+    block_size, postfix_len = discover_block_size_and_postfix_length(enc)
+    assert block_size == 16
+
+    mode = detector(enc)
+    assert mode == "ECB"
+
+    print("Postfix length determined to be", postfix_len)
+    blocks_needed = (postfix_len // block_size) + 1
+    bytes_needed = 16 * blocks_needed
+    slop = block_size - (postfix_len % block_size)
+    plaintext = b''
+
+    prefix = b'A' * bytes_needed
+
+    while len(prefix) > slop:
+        prefix = prefix[:-1]
+        ciphertext = enc(prefix)
+        for b in range(256):
+            byte = bytes([b])
+            candidate = enc(prefix + plaintext + byte)
+            if candidate[:bytes_needed] == ciphertext[:bytes_needed]:
+                plaintext += byte
+                break
+
+        # uncomment this line to watch the decryption character-by-character:
+        #print(plaintext)
+
+
+    print("Plaintext:\n")
+    print(postfix.decode("ascii"))
