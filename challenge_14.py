5cc862b226711dd2831608d8f2f0a6c476408a2f
Eli Sohl <elisohl@gmail.com>
2019-05-07 20:05:04 -0400

    challenge 14 complete

--- challenge_14.py
+++ challenge_14.py
@@ -1,4 +1,4 @@
-from challenge_12 import postfix
+from challenge_12 import secret_postfix
 
 from random import randint
 from os.path import commonprefix
@@ -9,15 +9,16 @@ from itertools import count
 from Crypto.Cipher import AES
 
 from challenge_09 import pkcs7
+from challenge_12 import secret_postfix
 
 
 key = urandom(16)
-prefix_length = randint(1, 34)  # lucky number 34
-prefix = urandom(prefix_length)
+secret_prefix_length = randint(1, 34)  # lucky number 34
+secret_prefix = urandom(secret_prefix_length)
 
 
 def enc(plaintext: bytes) -> bytes:
-    plaintext = pkcs7(prefix + plaintext + postfix)
+    plaintext = pkcs7(secret_prefix + plaintext + secret_postfix)
     cipher = AES.new(key, AES.MODE_ECB)
     return cipher.encrypt(plaintext)
 
@@ -38,7 +39,7 @@ def discover_prefix_length(enc: Callable[[bytes], bytes], block_size: int) -> in
     c1 = enc(b'A'*16)
     c2 = enc(b'B'*16)
     ind = block_size * (1 + len(commonprefix((c1, c2))) // block_size)
-    for i in range(16):
+    for i in range(1, 17):
         c3 = enc(b'A'*i + b'B'*16)
         if c1[:ind] == c3[:ind]:
             return ind - i
@@ -46,8 +47,51 @@ def discover_prefix_length(enc: Callable[[bytes], bytes], block_size: int) -> in
 
 
 if __name__ == "__main__":
-    print(prefix_length)
     block_size, affix_length = discover_block_size_and_affix_length(enc)
-    print(block_size, affix_length)
+    print("Discovered block size:", block_size)
+    assert block_size == 16
+
     prefix_length = discover_prefix_length(enc, block_size)
-    print(prefix_length, affix_length-prefix_length)
+    print("Discovered length for prefix:", prefix_length)
+    assert prefix_length == secret_prefix_length
+
+    postfix_len = affix_length - prefix_length
+    print("Discovered length for suffix:", postfix_len)
+    assert postfix_len == 138  # value discovered during challenge 12
+
+    #print(block_size, affix_length)
+    #print(discovered_prefix_length, affix_length-prefix_length)
+
+    prefix_len_to_add = block_size - (prefix_length % block_size)
+    prefix_junk_blocks_len = prefix_length + prefix_len_to_add
+
+    print("Prepending", prefix_len_to_add, "bytes to produce",
+            prefix_junk_blocks_len, "total bytes of leading garbage")
+
+    # copied over directly from challenge_12
+    # (only modifications: mixing in prefix_* variables in a few places)
+    blocks_needed = (postfix_len // block_size) + 1
+    bytes_needed = block_size*blocks_needed
+    slop = block_size - (postfix_len % block_size)
+    plaintext = b''
+
+    cutoff_ind = bytes_needed + prefix_junk_blocks_len
+
+    prefix = b'A' * (prefix_len_to_add + bytes_needed)
+
+    while len(prefix) > slop + prefix_len_to_add:
+        prefix = prefix[:-1]
+        ciphertext = enc(prefix)
+        for b in range(256):
+            byte = bytes([b])
+            candidate = enc(prefix + plaintext + byte)
+            if candidate[:cutoff_ind] == ciphertext[:cutoff_ind]:
+                plaintext += byte
+                break
+
+        # uncomment this line to watch the decryption character-by-character:
+        #print(plaintext)
+
+
+    print("Plaintext:\n")
+    print(plaintext.decode("ascii"))
