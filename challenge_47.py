660d478c2358e74c7b84edf7d1927989e624632d
Eli Sohl <elisohl@gmail.com>
2019-09-02 23:11:50 -0400

    added challenge 47!

--- challenge_47.py
+++ challenge_47.py
@@ -1,68 +1,125 @@
-from challenge_39 import RSA
+from challenge_39 import RSA, invmod
 
 from itertools import count
 from random import randrange
-from sys import urandom
+from math import floor, ceil
+from os import urandom
+
+
+# ref: http://archiv.infsec.ethz.ch/education/fs08/secsem/Bleichenbacher98.pdf
 
 
 def pkcs1(D: bytes):
-    assert len(D) <= k-11
+    assert len(D) <= 32-11  # we need len(ps) >= 8, meaning len(d) <= 32 - (8+3)
     ps = urandom(32 - (3+len(D)))
     return b'\x00\x02' + ps + b'\x00' + D
+
+
+def intersect(r1, r2):
+    # textbook algorithm. returns the intersection of the ranges in r1 and r2,
+    # where r1 and r2 are ordered lists of ordered pairs
+    result = []
+    i, j = 0, 0
+    while i < len(r1) and j < len(r2):      # look for overlaps
+        a, b = r1[i]
+        c, d = r2[j]
+        if b < d: i += 1
+        else: j += 1
+        if b >= c and d >= a:
+            result.append(sorted((a,b,c,d))[1:3])
+    for ri in range(len(result)-1):         # merge adjacent ranges
+        if 0 <= result[ri+1][0] - result[ri][1] <= 1:
+            result[ri:r1+2] = [[result[ri][0], result[ri+1][1]]]
+    return result
 
 
 if __name__ == "__main__":
     B = 2 ** (8 * (32-2))
     RSA.default_prime_size = 128
-    r = RSA()
-
-    def oracle(ct: int):
-        pt = r.dec(ct).to_bytes(32, 'big')
-        return pt.startswith(b'\x00\x02')
+    rsa = RSA()
 
     _m = int.from_bytes(pkcs1(b'kick it, CC'), 'big')
-    c = r.enc(_m)
+    c = rsa.enc(_m)
+
+
+    #### helpers
+
+    def oracle(ct: int):
+        pt = rsa.dec(ct).to_bytes(32, 'big')
+        return pt.startswith(b'\x00\x02')
+
+    def trial(pt: int):
+        return oracle((rsa.enc(pt) * c) % rsa.n)
+
+    assert oracle(c)
+
+
+    #### attack primitives
+
+    def step_2a(c_0):
+        lb = ceil(rsa.n / (3*B))
+        for s in count(lb):
+            if trial(s):
+                return s
+
+    def step_2b(s_i):
+        for s in count(s_i+1):
+            if trial(s):
+                return s
+
+    def step_2c(M, s):  # the s arg here is s_{i-1} in the paper's notation
+        a, b = M[0]
+        assert a < b
+        r_lb = (2*(b*s - 2*B) + rsa.n - 1) // rsa.n
+        for r in count(r_lb):
+            s_lb = (2*B + r*rsa.n) // b
+            s_ub = (3*B + r*rsa.n) // a
+            for s in range(s_lb, s_ub + 1):
+                if trial(s):
+                    return s
+
+    def step_3(M, s_i):
+        ranges = []
+        for a, b in M:
+            r_lo = (a*s_i - 3*B + 1) // rsa.n
+            r_hi = (b*s_i - 2*B) // rsa.n
+            for r in range(r_lo, r_hi+1):
+                #r_a = ceil((2*B + r*rsa.n) / s_i)
+                #r_b = floor((3*B - 1 + r*rsa.n) / s_i)
+                r_a = (2*B + r*rsa.n + s_i - 1) // s_i
+                r_b = (3*B - 1 + r*rsa.n) // s_i
+                ranges.append((r_a, r_b))
+                r += 1
+        return intersect(M, ranges)
+
+    def step_4(M):
+        if len(M) == 1 and M[0][0] == M[0][1]:
+            return M[0][0]  # = (a * invmod(s_0, rsa.n)) % rsa.n since s_0 = 1
+
 
     #### attack begins
 
-    # step 1
-    while True:
-        s0 = randrange(2**255)
-        c0 = (c * r.enc(s0)) % r.n
-        if oracle(c0): break
+    s_0 = 1  # step_1() not necessary b/c c is PKCS1-conforming
+    c_0 = (c * rsa.enc(s_0)) % rsa.n
+    M = [(2*B, 3*B-1)]
 
-    s_i = [s0]
-    c_i = [c0]
-    M_i = [{(2*B, 3*B-1)}]
-    i = 1
+    print("Starting search.\n")
 
-    # steps 2 thru 4
-    while True:
-        # step 2.a
-        if i == 1:
-            for s1 in count(r.n//(3*B)):
-                if oracle((c0 * r.enc(s1)) % r.n):
-                    break
-            s_i.append(s1)
+    for i in count(1):
+        print(end=".", flush=True)
 
-        else:
-            # step 2.b
-            if len(M_i[i-1]) > 1:
-                raise Exception("TODO")
+        if i == 1: s_i = step_2a(c_0)
+        elif len(M) > 1: s_i = step_2b(s_i)
+        else: s_i = step_2c(M, s_i)
+        #print(i, s_i)
 
-            # step 2.c
-            else:
-                raise Exception("TODO")
+        M = step_3(M, s_i)
+        #print(i, M)
 
-        # step 3
-        raise Exception("TODO")
+        m = step_4(M)
+        if m is not None: break
+        #if i >= 40: exit()
 
-        # step 4
-        if len(M_i[i]) == 1:
-            a, b = M_i[i][0]
-            if a == b:
-                m = (a * invmod(s0, r.n)) % r.n
-                break
-        i += 1
-
+    print("\n\n")
+    print("Recovered (padded) message:", m.to_bytes(32, 'big'))
     assert m == _m
