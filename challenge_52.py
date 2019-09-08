314e6791d254f8c40c628d2ba3359d72b49aba42
Eli Sohl <elisohl@gmail.com>
2019-09-08 04:09:46 -0400

    another const for shit hash's msg block size

--- challenge_52.py
+++ challenge_52.py
@@ -6,18 +6,19 @@ from challenge_09 import pkcs7
 from itertools import count, product
 
 
+M_BLOCK_SIZE = 8
 H_SIZE = 4
 H_INITIAL = b'\x00'*H_SIZE
 
 
 def C(M_i, H):
-    # assumptions: len(M_i) == 8, len(H) == H_SIZE
+    # assumptions: len(M_i) == M_BLOCK_SIZE, len(H) == H_SIZE
     cipher = Blowfish.new(H, Blowfish.MODE_ECB)  # note: we do take a performance hit here (AES sets up faster than Blowfish)
     return cipher.encrypt(M_i)[:H_SIZE]
 
 
 def MD(M, H=H_INITIAL, C=C):
-    blocks = bytes_to_chunks(pkcs7(M, 8), 8)
+    blocks = bytes_to_chunks(pkcs7(M, M_BLOCK_SIZE), M_BLOCK_SIZE)
     for block in blocks:
         H = C(block, H)
     return H
@@ -31,7 +32,7 @@ if __name__ == "__main__":
     H = H_INITIAL
     for i in count(1):
         outputs = {}
-        for j, candidate in enumerate(product(range(256), repeat=8)):
+        for j, candidate in enumerate(product(range(256), repeat=M_BLOCK_SIZE)):
             #if j % 10000 == 0: print(end=".", flush=True)
             m1 = bytes(candidate)
             out = C(m1, H)
