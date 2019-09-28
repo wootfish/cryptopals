61c481ca862441fab331cfb529710fb85fcc07c1
Eli Sohl <elisohl@gmail.com>
2019-09-27 21:17:15 -0400

    bugfix in challenge_04.py

--- challenge_04.py
+++ challenge_04.py
@@ -1,23 +1,20 @@
-"""
-Set 1, Challenge 3
-
-Scores 
-"""
-
-
 from challenge_03 import crack_xor_cipher
+
+from pprint import pprint
 
 
 if __name__ == "__main__":
-    with open("data/challenge_04.txt") as f:
+    with open("data/04.txt") as f:
         lines = [bytes.fromhex(line.strip()) for line in f]
 
-    overall_best = (-1, None, None, None)
+    overall_best = (float('inf'), None, None, None)
 
     for line in lines:
+        print(end='.', flush=True)
         result = crack_xor_cipher(line)
-        if result[0] > overall_best[0]:
+        if result[0] < overall_best[0]:
             overall_best = result + (line,)
+    print()
 
     score, key, plaintext, ciphertext = overall_best
     print("Most likely candidate ciphertext:", ciphertext)
